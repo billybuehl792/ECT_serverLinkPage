c17eb839b0fe25e83fc6d80534daa48f3a990bc6
Billy Buehl <billybuehl792@gmail.com>
2020-03-31 19:21:21 -0400

    working web UI

---
+++ app/app.js
@@ -0,0 +1,37 @@
+function dropDown(){
+    const contentItems = document.querySelectorAll('.contentContainer .contentItem')
+
+    contentItems.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            var slider = item.querySelector('.slider');
+
+            slider.classList.add('sliderActive');
+            textAlter(item, false);
+        });
+        item.addEventListener('mouseout', () => {
+            var slider = item.querySelector('.slider');
+
+            slider.classList.remove('sliderActive');
+            textAlter(item, true)
+        });
+    });
+}
+
+function textAlter(item, mode){
+    var listItems = item.querySelectorAll('.serverText .textContainer .listItem');
+    
+    if (mode){
+        listItems.forEach(listElem => {
+            listElem.classList.remove('listOn');
+        });
+    } else {
+        listItems.forEach(listElem => {
+            listElem.classList.add('listOn');
+        });
+    }
+}
+
+
+const app = () => {
+    dropDown();
+}
